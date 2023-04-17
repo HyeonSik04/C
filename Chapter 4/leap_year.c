6a1721116482e956329c64086b89875791db2cb5
HyeonSik04 <71442096+HyeonSik04@users.noreply.github.com>
2023-04-17 14:08:06 +0900

    Update leap_year.c

---
+++ Chapter 4/leap_year.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int year, result;
+	printf("년도를 입력하시오 : ");
+	scanf("%d", &year);
+	(year % 4 == 0) && (year / 100 != 0) || (year / 400 == 0) ? printf("%d년도는 윤년입니다", year) : printf("%d년도는 윤년이 아닙니다", year);
+	return 0;
+}
