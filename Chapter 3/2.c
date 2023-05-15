f6cea8cbfe272714bd247d2f47e88b92c56a4792
HyeonSik04 <71442096+HyeonSik04@users.noreply.github.com>
2023-05-15 15:04:12 +0900

    Update 2.c

--- Chapter 3/2.c
+++ Chapter 3/2.c
@@ -3,12 +3,12 @@
 int main() {
 	double mile,meter;
 
-	printf("마일을 입력하시오: "); //마일 값 받기
+	printf("마일을 입력하시오: ");
 	scanf("%lf", &mile);
 
-	meter = mile * 1609; //미터 계산
+	meter = mile * 1609;
 
-	printf("%.1lf 마일은 %.2lf미터 입니다", mile, meter); //입력 받은 마일 값과 계산된 미터 값 출력
+	printf("%.1lf 마일은 %.2lf미터 입니다", mile, meter);
 
 	return 0;
 }
