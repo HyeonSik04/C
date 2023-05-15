98013a1ecc26054d6cc7831b13456aa6c9497abe
HyeonSik04 <71442096+HyeonSik04@users.noreply.github.com>
2023-05-15 15:03:55 +0900

    Update 1.c

--- Chapter 3/1.c
+++ Chapter 3/1.c
@@ -3,12 +3,12 @@
 int main() {
 	double YearMoney, year;
 
-	printf("연봉을 입력하시오(단위: 만원): "); //연봉 입력받기
+	printf("연봉을 입력하시오(단위: 만원): ");
 	scanf("%lf", &YearMoney);
 
-	year = 100000 / YearMoney; //10억을 연봉으로 나눠 10억 모으는 시간 계산
+	year = 100000 / YearMoney;
 
-	printf("10억을 모으는데 걸리는 시간(단위: 년) : %.2lf", year); //모으는 시간 출력
+	printf("10억을 모으는데 걸리는 시간(단위: 년) : %.2lf", year);
 
 	return 0;
 }
