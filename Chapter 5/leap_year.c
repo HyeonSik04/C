1cfc74be0e768bc8c1b46c0e27c438f17d233d71
HyeonSik04 <71442096+HyeonSik04@users.noreply.github.com>
2023-05-15 14:41:19 +0900

    Update leap_year.c

--- Chapter 5/leap_year.c
+++ Chapter 5/leap_year.c
@@ -2,11 +2,11 @@
 
 int main() {
 	int year;
-  
-	printf("년도를 입력하시오 : ");
+	
+	printf("연도를 입력하시오 : ");
 	scanf("%d", &year);
-  
-	((year % 4 == 0) && (year / 100 != 0) || (year / 400 == 0)) ? printf("%d년도는 윤년입니다", year) : printf("%d년도는 윤년이 아닙니다", year);
-  
+
+	((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) ? printf("윤년입니다.") : printf("윤년이 아닙니다.");
+
 	return 0;
 }
