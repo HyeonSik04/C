1bb595aa076441f5a5bc0b7f6c55fa75d8e63a3f
HyeonSik04 <71442096+HyeonSik04@users.noreply.github.com>
2023-05-15 15:03:41 +0900

    Update 5.c

--- Chapter 3/5.c
+++ Chapter 3/5.c
@@ -3,13 +3,12 @@
 int main() {
 	double x, polynomial;
 
-	printf("실수를 입력하시오: "); //x 값 받기
+	printf("실수를 입력하시오: ");
 	scanf("%lf", &x);
 
+	polynomial = 3 * x * x + 7 * x + 11;
 
-	polynomial = 3 * x * x + 7 * x + 11; //받은 x값으로 다항식 계산
-
-	printf("다항식의 값은 %.2lf", polynomial); //다항식의 값 출력
+	printf("다항식의 값은 %.2lf", polynomial);
 
 	return 0;
 }
