e15baaba2f449d9c28f1b4c618366f92cd00ae40
HyeonSik04 <71442096+HyeonSik04@users.noreply.github.com>
2023-05-15 15:05:37 +0900

    Update 3.c

--- Chapter 3/3.c
+++ Chapter 3/3.c
@@ -3,15 +3,15 @@
 int main() {
 	double Width, Height, Size;
 
-	printf("삼각형의 밑변: "); //삼각형 밑변 받기
+	printf("삼각형의 밑변: ");
 	scanf("%lf", &Width);
 
-	printf("삼각형의 높이: "); //삼각형 높이 받기
+	printf("삼각형의 높이: ");
 	scanf("%lf",&Height);
 
-	Size = Width * Height/2; //삼각형 넓이 계산
+	Size = Width * Height/2;
 
-	printf("삼각형의 넓이: %.2lf",Size); //넓이 출력
+	printf("삼각형의 넓이: %.2lf",Size);
 
 	return 0;
 }
