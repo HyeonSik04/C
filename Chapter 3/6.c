e8b6c489028269ff0bca86a43480618e2eaac49a
HyeonSik04 <71442096+HyeonSik04@users.noreply.github.com>
2023-05-15 15:04:28 +0900

    Update 6.c

--- Chapter 3/6.c
+++ Chapter 3/6.c
@@ -3,12 +3,12 @@
 int main() {
 	double weight,moon_weight; 
 
-	printf("몸무게를 입력하시오(단위: kg): "); //몸무게 입력받기
+	printf("몸무게를 입력하시오(단위: kg): ");
 	scanf("%lf", &weight);
 
-	moon_weight = 70 * 0.17; //달에서의 몸무게 계산
+	moon_weight = 70 * 0.17;
 
-	printf("달에서의 몸무게는 %.2lf입니다.", moon_weight); //달에서의 몸무게 출력
+	printf("달에서의 몸무게는 %.2lf입니다.", moon_weight);
 
 	return 0;
 }
