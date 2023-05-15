4e6993a7d70676ff6861f650e59245113fb37bfd
HyeonSik04 <71442096+HyeonSik04@users.noreply.github.com>
2023-05-15 15:04:41 +0900

    Update 4.c

--- Chapter 3/4.c
+++ Chapter 3/4.c
@@ -3,12 +3,12 @@
 int main() {
 	double F, C;
 
-	printf("화씨 값을 입력하시오: "); //화씨 값 받기
+	printf("화씨 값을 입력하시오: ");
 	scanf("%lf", &F);
 
 	C = (F - 32)*5/9; //섭씨 계산
 
-	printf("섭씨값은 %.2lf도 입니다.", C); //섭씨 출력
+	printf("섭씨값은 %.2lf도 입니다.", C);
 
 	return 0;
 }
