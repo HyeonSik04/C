fe8af02c3d94267f4ee5c9a5f33cf82d8872a318
HyeonSik04 <71442096+HyeonSik04@users.noreply.github.com>
2023-06-27 18:59:33 +0900

    Update 1.c

--- Chapter 7/loop/1.c
+++ Chapter 7/loop/1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	int length, i, j;
+	int length;
 	
 	scanf("%d", &length);
 
-	for (i = 1; i <= length; i++) {
-		for (j = 1; j <= i; j++) {
+	for (int i = 1; i <= length; i++) {
+		for (int j = 1; j <= i; j++) {
 			printf("*");
 		}
 		printf("\n");
